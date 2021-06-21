95ba1cef5b4043c1f519f4ea9a2297fc0b7e1c53
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2021-06-21 12:19:00 +0700

    penyesuaian WH.sql dengan star schema sesuai catatan

--- WH.sql
+++ WH.sql
@@ -4,42 +4,6 @@ use AdventureWH6;
 -- masuk ke AdventureWH6 terlebih dahulu!!! sebelum melanjutkan
 create schema WH;
 
--- -- product category
--- create table WH.ProductCategory
--- (
--- 	ProductCategoryKey int identity
--- 		constraint ProductCategory_pk
--- 			primary key nonclustered,
--- 	ProductCategoryID int not null,
--- 	ProductCategoryName nvarchar(50)
--- )
---
--- -- product sub category
--- create table WH.ProductSubCategory
--- (
--- 	ProductSubCategoryKey int identity
--- 		constraint ProductSubCategory_pk
--- 			primary key nonclustered,
--- 	ProductSubCategoryID int not null,
--- 	ProductSubCategoryName nvarchar(50),
--- 	ProductCategoryKey int not null
--- 		constraint ProductSubCategory_ProductCategory_ProductCategoryKey_fk
--- 			references WH.ProductCategory
--- )
---
--- -- product
--- create table WH.Product
--- (
--- 	ProductKey int identity
--- 		constraint Product_pk
--- 			primary key nonclustered,
--- 	ProductID int not null,
--- 	ProductName nvarchar(50),
--- 	ProductSubCategoryKey int
--- 		constraint Product_ProductSubCategory_ProductSubCategoryKey_fk
--- 			references WH.ProductSubCategory
--- )
-
 -- time
 create table WH.Time
 (
@@ -52,18 +16,6 @@ create table WH.Time
 	DayOfMonth int,
 	DayName nvarchar(10),
 	Date datetime
-)
-
--- territory
-create table WH.Territory
-(
-	TerritoryKey int identity
-		constraint Territory_pk
-			primary key nonclustered,
-	TerritoryID int,
-	TerritoryName nvarchar(50),
-	CountryRegionCode nvarchar(3),
-	[Group] nvarchar(50)
 )
 
 -- Department
@@ -116,15 +68,6 @@ create table WH.Shift
 	EndTime time(7)
 )
 
--- -- Bio
--- create table WH.Bio
--- (
--- 	BioKey int identity
--- 		constraint Bio_pk
--- 			primary key nonclustered,
---     BusinessEntityID int,
--- )
-
 -- Employee
 create table WH.Employee
 (
@@ -132,9 +75,6 @@ create table WH.Employee
 		constraint Employee_pk
 			primary key nonclustered,
     BusinessEntityID int,
-	SalesQuota money,
-	SalesYTD money,
-	CommissionPct smallmoney,
 	PayRate money,
 	PayFrequency int,
 	SickLeaveHours int,
@@ -142,13 +82,13 @@ create table WH.Employee
 	BirthDate date,
 	HireDate date,
 	MaritalKey int
-		constraint Bio_Marital_MaritalKey_fk
+		constraint Employee_Marital_MaritalKey_fk
 			references WH.Marital,
 	GenderKey int
-		constraint Bio_Gender_GenderKey_fk
+		constraint Employee_Gender_GenderKey_fk
 			references WH.Gender,
 	TitleKey int
-		constraint Bio_Title_TitleKey_fk
+		constraint Employee_Title_TitleKey_fk
 			references WH.Title,
 	FirstName nvarchar(50),
 	MiddleName nvarchar(50),
@@ -158,23 +98,12 @@ create table WH.Employee
 			references WH.Shift,
 	DepartmentKey int
 		constraint Employee_Department_DepartmentKey_fk
-			references WH.Department,
-	BioKey int
-		constraint Employee_Bio_BioKey_fk
-			references WH.Bio,
-	TerritoryKey int
-		constraint Employee_Territory_TerritoryKey_fk
-			references WH.Territory
+			references WH.Department
 )
 
 -- FactTable
 create table WH.FactTable
 (
--- 	SalesOrderID int not null,
--- 	SalesOrderDetailID int not null,
--- 	ProductKey int
--- 		constraint FactTable_Product_ProductKey_fk
--- 			references WH.Product,
 	EmployeeKey int
 		constraint FactTable_Employee_EmployeeKey_fk
 			references WH.Employee,
@@ -183,5 +112,3 @@ create table WH.FactTable
 			references WH.Time,
 	TotalWork money,
 )
-
--- alter table WH.FactTable ADD CONSTRAINT PK_FACTID PRIMARY KEY (SalesOrderID, SalesOrderDetailID)
