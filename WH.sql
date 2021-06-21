670814bb3d1c7b86f34464e8a0426f860ef595c7
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2021-06-21 12:08:19 +0700

    delete product dimension, change measurement to TotalWork, make Bio and Employee to be 1 table

--- WH.sql
+++ WH.sql
@@ -1,43 +1,44 @@
 create database AdventureWH6;
+use AdventureWH6;
 
 -- masuk ke AdventureWH6 terlebih dahulu!!! sebelum melanjutkan
 create schema WH;
 
--- product category
-create table WH.ProductCategory
-(
-	ProductCategoryKey int identity
-		constraint ProductCategory_pk
-			primary key nonclustered,
-	ProductCategoryID int not null,
-	ProductCategoryName nvarchar(50)
-)
-
--- product sub category
-create table WH.ProductSubCategory
-(
-	ProductSubCategoryKey int identity
-		constraint ProductSubCategory_pk
-			primary key nonclustered,
-	ProductSubCategoryID int not null,
-	ProductSubCategoryName nvarchar(50),
-	ProductCategoryKey int not null
-		constraint ProductSubCategory_ProductCategory_ProductCategoryKey_fk
-			references WH.ProductCategory
-)
-
--- product
-create table WH.Product
-(
-	ProductKey int identity
-		constraint Product_pk
-			primary key nonclustered,
-	ProductID int not null,
-	ProductName nvarchar(50),
-	ProductSubCategoryKey int
-		constraint Product_ProductSubCategory_ProductSubCategoryKey_fk
-			references WH.ProductSubCategory
-)
+-- -- product category
+-- create table WH.ProductCategory
+-- (
+-- 	ProductCategoryKey int identity
+-- 		constraint ProductCategory_pk
+-- 			primary key nonclustered,
+-- 	ProductCategoryID int not null,
+-- 	ProductCategoryName nvarchar(50)
+-- )
+--
+-- -- product sub category
+-- create table WH.ProductSubCategory
+-- (
+-- 	ProductSubCategoryKey int identity
+-- 		constraint ProductSubCategory_pk
+-- 			primary key nonclustered,
+-- 	ProductSubCategoryID int not null,
+-- 	ProductSubCategoryName nvarchar(50),
+-- 	ProductCategoryKey int not null
+-- 		constraint ProductSubCategory_ProductCategory_ProductCategoryKey_fk
+-- 			references WH.ProductCategory
+-- )
+--
+-- -- product
+-- create table WH.Product
+-- (
+-- 	ProductKey int identity
+-- 		constraint Product_pk
+-- 			primary key nonclustered,
+-- 	ProductID int not null,
+-- 	ProductName nvarchar(50),
+-- 	ProductSubCategoryKey int
+-- 		constraint Product_ProductSubCategory_ProductSubCategoryKey_fk
+-- 			references WH.ProductSubCategory
+-- )
 
 -- time
 create table WH.Time
@@ -115,28 +116,14 @@ create table WH.Shift
 	EndTime time(7)
 )
 
--- Bio
-create table WH.Bio
-(
-	BioKey int identity
-		constraint Bio_pk
-			primary key nonclustered,
-    BusinessEntityID int,
-	BirthDate date,
-	HireDate date,
-	MaritalKey int
-		constraint Bio_Marital_MaritalKey_fk
-			references WH.Marital,
-	GenderKey int
-		constraint Bio_Gender_GenderKey_fk
-			references WH.Gender,
-	TitleKey int
-		constraint Bio_Title_TitleKey_fk
-			references WH.Title,
-	FirstName nvarchar(50),
-	MiddleName nvarchar(50),
-	LastName nvarchar(50)
-)
+-- -- Bio
+-- create table WH.Bio
+-- (
+-- 	BioKey int identity
+-- 		constraint Bio_pk
+-- 			primary key nonclustered,
+--     BusinessEntityID int,
+-- )
 
 -- Employee
 create table WH.Employee
@@ -152,6 +139,20 @@ create table WH.Employee
 	PayFrequency int,
 	SickLeaveHours int,
 	VacationHours int,
+	BirthDate date,
+	HireDate date,
+	MaritalKey int
+		constraint Bio_Marital_MaritalKey_fk
+			references WH.Marital,
+	GenderKey int
+		constraint Bio_Gender_GenderKey_fk
+			references WH.Gender,
+	TitleKey int
+		constraint Bio_Title_TitleKey_fk
+			references WH.Title,
+	FirstName nvarchar(50),
+	MiddleName nvarchar(50),
+	LastName nvarchar(50),
 	ShiftKey int
 		constraint Employee_Shift_ShiftKey_fk
 			references WH.Shift,
@@ -169,18 +170,18 @@ create table WH.Employee
 -- FactTable
 create table WH.FactTable
 (
-	SalesOrderID int not null,
-	SalesOrderDetailID int not null,
-	ProductKey int
-		constraint FactTable_Product_ProductKey_fk
-			references WH.Product,
+-- 	SalesOrderID int not null,
+-- 	SalesOrderDetailID int not null,
+-- 	ProductKey int
+-- 		constraint FactTable_Product_ProductKey_fk
+-- 			references WH.Product,
 	EmployeeKey int
 		constraint FactTable_Employee_EmployeeKey_fk
 			references WH.Employee,
 	TimeKey int
 		constraint FactTable_Time_TimeKey_fk
 			references WH.Time,
-	TotalSales money,
+	TotalWork money,
 )
 
-alter table WH.FactTable ADD CONSTRAINT PK_FACTID PRIMARY KEY (SalesOrderID, SalesOrderDetailID)
+-- alter table WH.FactTable ADD CONSTRAINT PK_FACTID PRIMARY KEY (SalesOrderID, SalesOrderDetailID)
